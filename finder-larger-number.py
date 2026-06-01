b23f4b402cda83df537a2e0372e42411d0f4fe34
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-06-01 20:58:43 +0530

    finder-larger-number.py

---
+++ finder-larger-number.py
@@ -0,0 +1,9 @@
+a = float (input ("Enter first number :"))
+b = float (input("Enter second number :"))
+
+if a>b :
+	print ("first number is larger :",a)
+elif b>a:
+	print ("second number is larger : ",b)
+else :
+	print ("both number are equal")             
