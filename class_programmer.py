0929b107c394c5fa9f6bbb70b0598427a125a745
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-06-04 11:55:48 +0530

    class_programmer.py

---
+++ class_programmer.py
@@ -0,0 +1,16 @@
+class programmer:
+	company = "microsoft"
+	def __init__(self,name,salary,pin):
+		self.name = name 
+		self.salary = salary
+		self .pin = pin  
+		
+		
+p = programmer ("zoya",1200000,245002)
+print (p.name ,p.salary ,p.pin,p.company)
+
+ 
+ 
+		
+r = programmer ("ranu",1200000,245002)
+print (r.name ,r.salary ,r.pin,r.company)
