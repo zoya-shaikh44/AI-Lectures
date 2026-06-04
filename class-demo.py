5788fc42c156c0b329251c8a264cee28f5602d2a
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-06-04 12:23:46 +0530

    Create class-demo.py

---
+++ class-demo.py
@@ -0,0 +1,11 @@
+# class attributes
+class demo :
+	a = 4 
+	
+	
+o = demo ()
+print (o.a) # print the class attribute because instance attribute is not present
+print (o.a ) #instance attribute is set 
+o.a = 0 # print the instance attributes because instance attributes is present
+print (o.a)
+print(demo.a )#print th eclass attributes 
